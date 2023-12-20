74f080cc15efc959b0f714900f6b61d684abd0a5
Pranav Dronavalli <62522813+dronavallipranav@users.noreply.github.com>
2023-12-20 17:10:29 -0600

    updated landing page styling (#2)
    Co-authored-by: davidteather <34144122+davidteather@users.noreply.github.com>

--- website/src/components/Landing/Hero.tsx
+++ website/src/components/Landing/Hero.tsx
@@ -1,6 +1,7 @@
+import Link from "next/link";
 export default function Hero() {
   return (
-    <header className="flex flex-wrap items-center justify-between px-10 py-20 bg-gray-100">
+    <header className="flex flex-wrap items-center justify-between px-20 py-20 bg-gray-100">
       <div className="w-full lg:w-1/2">
         <h1 className="text-5xl font-bold leading-tight mb-4">
           Transform your Hackathon experience with ApplicantAtlas
@@ -10,18 +11,20 @@ export default function Hero() {
           events and applicant tracking.
         </p>
         <div className="flex space-x-4">
+          <Link href = "/register">
           <button className="btn btn-primary py-2 px-4 rounded-md shadow-lg">
             Get Started
           </button>
+          </Link>
           <button className="btn btn-outline btn-secondary py-2 px-4 rounded-md shadow-lg">
             Learn More
           </button>
         </div>
       </div>
-      <div className="w-full lg:w-1/2 lg:flex lg:justify-end mt-8 lg:mt-0">
+      <div className="w-full lg:w-1/2 lg:flex lg:justify-end mt-8 px-2 lg:mt-0">
         <img
-          src="https://via.placeholder.com/400x400"
-          alt="Placeholder"
+          src="/assets/index/tower.jpg"
+          alt="Hero Tower"
           className="rounded-lg shadow-lg object-cover object-center w-full h-auto max-w-sm lg:max-w-md"
         />
       </div>
