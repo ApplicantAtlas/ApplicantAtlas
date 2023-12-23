ed8f32c7bef1ffddf1f38c373fc54b162f6ddb6c
davidteather <34144122+davidteather@users.noreply.github.com>
2023-12-22 20:22:36 -0500

    add mongo mock tests for events test and such

--- api/internal/mocks/mongo_mock_service.go
+++ api/internal/mocks/mongo_mock_service.go
@@ -4,6 +4,7 @@ import (
 	"api/internal/models"
 	"api/internal/mongodb"
 	"context"
+	"errors"
 	"sync"
 
 	"github.com/gin-gonic/gin"
@@ -15,13 +16,15 @@ import (
 
 type MockMongoService struct {
 	mock.Mock
-	data  map[string]models.User // In-memory store
-	mutex sync.RWMutex           // Mutex for concurrent access
+	data   map[string]models.User // In-memory store
+	events map[string]models.Event
+	mutex  sync.RWMutex // Mutex for concurrent access
 }
 
 func NewMockMongoService() *MockMongoService {
 	return &MockMongoService{
-		data: make(map[string]models.User),
+		data:   make(map[string]models.User),
+		events: make(map[string]models.Event),
 	}
 }
 
@@ -61,23 +64,86 @@ func (m *MockMongoService) DeleteUserByEmail(ctx context.Context, email string) 
 }
 
 func (m *MockMongoService) CreateEvent(ctx context.Context, event models.Event) (*mongo.InsertOneResult, error) {
-	// TODO: Implement
-	return nil, nil
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	// Generate event.ID
+	event.ID = primitive.NewObjectID()
+
+	id := event.ID.Hex()
+	if _, exists := m.data[id]; exists {
+		return nil, errors.New("event already exists")
+	}
+
+	m.events[id] = event
+	return &mongo.InsertOneResult{InsertedID: event.ID}, nil
 }
 
 func (m *MockMongoService) UpdateEventMetadata(ctx *gin.Context, eventID primitive.ObjectID, metadata models.EventMetadata) (*mongo.UpdateResult, error) {
-	// TODO: Implement
-	return nil, nil
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	id := eventID.Hex()
+	event, exists := m.events[id]
+	if !exists {
+		return nil, mongo.ErrNoDocuments
+	}
+
+	// Update metadata
+	event.Metadata = metadata
+	m.events[id] = event
+	return &mongo.UpdateResult{ModifiedCount: 1}, nil
 }
 
 func (m *MockMongoService) ListEventsMetadata(ctx context.Context, filter bson.M) ([]models.Event, error) {
-	return nil, nil
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+
+	var events []models.Event
+	for _, event := range m.events {
+		if matchesFilter(event, filter) {
+			events = append(events, event)
+		}
+	}
+	return events, nil
 }
 
 func (m *MockMongoService) DeleteEvent(ctx *gin.Context, eventID primitive.ObjectID) (*mongo.DeleteResult, error) {
-	return nil, nil
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	id := eventID.Hex()
+	if _, exists := m.data[id]; !exists {
+		return nil, mongo.ErrNoDocuments
+	}
+
+	delete(m.data, id)
+	return &mongo.DeleteResult{DeletedCount: 1}, nil
 }
 
 func (m *MockMongoService) GetEvent(ctx *gin.Context, eventID primitive.ObjectID) (*models.Event, error) {
-	return nil, nil
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+
+	id := eventID.Hex()
+	event, exists := m.events[id]
+	if !exists {
+		return nil, mongo.ErrNoDocuments
+	}
+
+	return &event, nil
+}
+
+// matchesFilter is a helper function that matches an event against a filter.
+// This is a simplified example. You would need to implement matching logic
+// based on your application's requirements.
+func matchesFilter(event models.Event, filter bson.M) bool {
+	for k, v := range filter {
+		// Example of matching by event name
+		if k == "name" && event.Metadata.Name != v {
+			return false
+		}
+		// Add other filter conditions as needed
+	}
+	return true
 }
