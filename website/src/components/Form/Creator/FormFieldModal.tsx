4d640420c7129e0cda83e4f6ef92129dceef3f06
davidteather <34144122+davidteather@users.noreply.github.com>
2023-12-30 15:04:18 -0500

    add info on type selector modal

--- website/src/components/Form/Creator/FormFieldModal.tsx
+++ website/src/components/Form/Creator/FormFieldModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { humanFormTypes } from "../FormConstants";
+import { humanFormTypes, fieldDescriptions } from "../FormConstants"; // Assuming fieldDescriptions is an object mapping field types to their descriptions
 
 interface FormFieldModalProps {
   isOpen: boolean;
@@ -12,34 +12,53 @@ const FormFieldModal: React.FC<FormFieldModalProps> = ({
   onClose,
   onFieldSelect,
 }) => {
-    return (
-        <>
-          {isOpen && (
-            <div className="fixed inset-0 bg-gray-600 bg-opacity-50 z-50 flex justify-center items-center">
-              <div className="bg-white rounded-lg shadow-lg p-5 m-4 max-w-xl max-h-full overflow-auto">
-                <div className="mb-4">
-                  <h3 className="text-xl font-bold">Add a new field</h3>
-                  <p className="text-sm text-gray-600">
-                    Select the type of field you want to add to your form.
-                  </p>
-                </div>
-                <ul className="menu bg-base-100 w-full overflow-y-auto">
+  const [selectedFieldType, setSelectedFieldType] = useState<string | null>(null);
+
+  const handleFieldSelection = (type: string) => {
+    setSelectedFieldType(type);
+  };
+
+  const handleAddField = () => {
+    if (selectedFieldType) {
+      onFieldSelect(selectedFieldType);
+      onClose();
+    }
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <div className="modal modal-open">
+          <div className="modal-box relative">
+            <h3 className="text-xl font-bold">Add a new field</h3>
+            <div className="flex">
+              <div className="flex-1 p-4">
+                <ul className="menu bg-base-100 w-full">
                   {humanFormTypes.map((type) => (
-                    <li key={type} onClick={() => onFieldSelect(type)} className="hover:bg-gray-100 cursor-pointer">
-                      <a className="text-gray-900 capitalize">{type}</a>
+                    <li key={type} className={selectedFieldType === type ? "bordered" : ""}>
+                      <a onClick={() => handleFieldSelection(type)}>{type}</a>
                     </li>
                   ))}
                 </ul>
-                <div className="modal-action mt-4">
-                  <button onClick={onClose} className="btn btn-outline btn-accent w-full">
-                    Close
-                  </button>
-                </div>
+              </div>
+              <div className="flex-1 p-4">
+                {selectedFieldType && (
+                  <div>
+                    <h4 className="text-lg font-semibold">{selectedFieldType}</h4>
+                    <p>{fieldDescriptions[selectedFieldType]}</p>
+                  </div>
+                )}
               </div>
             </div>
-          )}
-        </>
-      );
+            <div className="modal-action">
+              <button onClick={handleAddField} className="btn btn-primary">Add Field</button>
+              <button onClick={onClose} className="btn btn-outline">Close</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default FormFieldModal;
