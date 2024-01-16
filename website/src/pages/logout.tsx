1c65d74c02ccbb502f74fdf69d6da35218367b00
davidteather <34144122+davidteather@users.noreply.github.com>
2024-01-16 16:15:44 -0600

    better login logic

--- website/src/pages/logout.tsx
+++ website/src/pages/logout.tsx
@@ -1,19 +1,45 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import AuthService from '@/services/AuthService';
+import { useRouter } from 'next/router';
 
 const LogoutPage = () => {
+    const [isLoggedOut, setIsLoggedOut] = useState(false);
     const router = useRouter();
 
     useEffect(() => {
-        AuthService.logout()
-        router.push('/');
+        const timer = setTimeout(() => {
+                router.push('/');
+            }, 3000);
+        return () => clearTimeout(timer);
     }, [router]);
 
+    useEffect(() => {
+        if (AuthService.isAuth()) {
+            handleLogout();
+        } else {
+            setIsLoggedOut(true);
+        }
+    }, []);
+
+    const handleLogout = async () => {
+        await AuthService.logout();
+        setIsLoggedOut(true);
+    };
+
+    if (isLoggedOut) {
+        return (
+            <div>
+                <p>You have been logged out. Redirecting you to the home page, or click below.</p>
+                <Link href="/">
+                    <span>Go to Home</span>
+                </Link>
+            </div>
+        );
+    }
+
     return (
-        <div>
-            <p>Logging out...</p>
-        </div>
+        <p>Logging you out...</p>
     );
 };
 
