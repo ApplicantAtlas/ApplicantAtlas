db88231bfc27f8370f689ee2a136d8c33c659a62
davidteather <34144122+davidteather@users.noreply.github.com>
2024-08-02 15:36:56 -0500

    Bump version: 0.0.7 → 0.0.8

--- backend/shared/utils/version.go
+++ backend/shared/utils/version.go
@@ -1,3 +1,3 @@
 package utils
 
-var VERSION = "0.0.7"
+var VERSION = "0.0.8"
--- website/next.config.js
+++ website/next.config.js
@@ -17,7 +17,7 @@ if (isProd) {
 const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: apiUrl,
-    NEXT_PUBLIC_VERSION: '0.0.7',
+    NEXT_PUBLIC_VERSION: '0.0.8',
 
     // PostHog analytics
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY || '',
